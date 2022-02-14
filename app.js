b309e14e77b47e1a37991d9716e09f875c4b3121
Roman Strypa <gfdg@bbbb.yyy>
2022-02-14 17:16:02 +0200

    Add: router, errorHandler

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const sequelize = require('./db')
-const models = require('./models/models')
-const cors = require('cors')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const cors = require('cors');
+const app = express();
 
 const connectToDB = async () => {
   try {
-    await sequelize.authenticate()
-    await sequelize.sync() 
-    console.log('Database connected')
+    await sequelize.authenticate();
+    await sequelize.sync(); 
+    console.log('Database connected');
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
-connectToDB()
+connectToDB();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+app.use(errorHandler);
 
 /*
 app.get('/', (req, res) => {
@@ -26,4 +30,4 @@ app.get('/', (req, res) => {
 */
 
 
-module.exports = app
+module.exports = app;
