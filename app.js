86cc5f3c29d96c8790901ec8ee359a4b853033d0
Roman Strypa <gfdg@bbbb.yyy>
2022-02-15 20:49:43 +0200

    Add: file upload

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index');
+const fileUpload = require('express-fileupload');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 
@@ -20,6 +22,8 @@ connectToDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler);
 
